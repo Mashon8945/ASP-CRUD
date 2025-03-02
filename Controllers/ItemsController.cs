e84648caea1ee45be2904ae48751dc92bd38c9e4
Le'Mashon <Leonardlemashon@gmail.com>
2025-03-02 18:31:18 +0300

    asp net mvc project

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly MyAppContext _context;
+        public ItemsController(MyAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var item = await _context.Items.ToListAsync();
+            return View(item);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        public async Task<IActionResult> Edit (int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x=>x.Id == id);
+            
+        }
+    }
+    
+}

d6d5dee84c841302d5e741d50eaf1992381bd3ad
Le'Mashon <Leonardlemashon@gmail.com>
2025-03-03 19:00:49 +0300

    rebase changes

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e84648caea1ee45be2904ae48751dc92bd38c9e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2921abd3e50a2bef39c22cadd5a6f113cb57598f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
